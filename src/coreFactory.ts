5e4e24487fddeb6b6d533f4f289b291d4a281582
Xavier Le Cunff <xavier@MacBook-Pro-de-Xavier.local>
2023-09-30 00:49:18 +0200

    wip: core constructor generated with error

--- src/coreFactory.ts
+++ src/coreFactory.ts
@@ -1,35 +1,46 @@
-// coreFactory.ts
-import { StoreType, DependenciesType } from './types';
 import { ServiceRegistry } from './base';
 
-export function createCoreFactory<ServiceClasses>(
+export function createCoreFactory<ServiceClasses, StoreType, DependenciesType>(
   store: StoreType,
-  dependencies: DependenciesType,
   serviceConstructors: {
     [K in keyof ServiceClasses]: new (
       store: StoreType,
       dependencies: DependenciesType,
-      serviceRegistry: ServiceRegistry<ServiceClasses>
+      serviceRegistry: ServiceRegistry<
+        ServiceClasses,
+        StoreType,
+        DependenciesType
+      >
     ) => ServiceClasses[K];
   }
 ) {
-  const serviceRegistry = new ServiceRegistry<ServiceClasses>(
-    store,
-    dependencies
-  );
+  return class Core {
+    private serviceRegistry: ServiceRegistry<
+      ServiceClasses,
+      StoreType,
+      DependenciesType
+    >;
 
-  for (const [key, ServiceConstructor] of Object.entries(serviceConstructors)) {
-    const instance = new ServiceConstructor(
-      store,
-      dependencies,
-      serviceRegistry
-    );
-    serviceRegistry.setInstance(key as keyof ServiceClasses, instance);
-  }
+    constructor(dependencies: Partial<DependenciesType>) {
+      this.serviceRegistry = new ServiceRegistry(
+        store,
+        dependencies as DependenciesType
+      );
 
-  return {
+      for (const [key, ServiceConstructor] of Object.entries(
+        serviceConstructors
+      )) {
+        const instance = new ServiceConstructor(
+          store,
+          dependencies as DependenciesType,
+          this.serviceRegistry
+        );
+        this.serviceRegistry.setInstance(key as keyof ServiceClasses, instance);
+      }
+    }
+
     getService<K extends keyof ServiceClasses>(name: K): ServiceClasses[K] {
-      return serviceRegistry.get(name);
-    },
+      return this.serviceRegistry.get(name);
+    }
   };
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { ServiceRegistry } from './base';
+
 export interface StoreType {
   user: { id: string; name: string };
   settings: { theme: string };
@@ -7,4 +9,10 @@ export interface DependenciesType {
   apiClient: any;
 }
 
-export type Constructor<T = {}> = new (...args: any[]) => T;
+export type ServiceConstructor<ServiceClasses, Store, Dependencies> = {
+  [K in keyof ServiceClasses]: new (
+    store: Store,
+    dependencies: Partial<Dependencies>,
+    serviceRegistry: ServiceRegistry<ServiceClasses, Store, Dependencies>
+  ) => ServiceClasses[K];
+};
