54edf35d51d7c398e3f795001bc15121c2dc2607
Xavier Le Cunff <xavier@MacBook-Pro-de-Xavier.local>
2023-09-30 16:37:26 +0200

    fix: delete useless UserService

--- src/UserService.ts
+++
@@ -1,9 +0,0 @@
-import { BaseService } from './BaseService';
-import { injectable } from 'inversify';
-
-@injectable()
-export class UserService<ServiceMapType> extends BaseService<ServiceMapType> {
-  getUser() {
-    console.log('Fetching user...');
-  }
-}
