8109810e5e9e8090becbfca2d3588efd3da7e084
Xavier Le Cunff <xavier@MacBook-Pro-de-Xavier.local>
2023-10-01 15:15:13 +0200

    doc: add documentation

--- doc/src/pages/index.tsx
+++ doc/src/pages/index.tsx
@@ -8,7 +8,7 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -17,8 +17,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/intro"
+          >
+            Get Started
           </Link>
         </div>
       </div>
@@ -27,11 +28,12 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="THe missing brick of React to code in TDD and clean architecture"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
