461df4548c84d896e5ab8a7c079de6d4a8935e02
Xavier Le Cunff <xavier@MacBook-Pro-de-Xavier.local>
2023-11-25 18:32:11 +0100

    feat: change logo

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -66,12 +66,12 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/logo_512.png',
       navbar: {
         title: 'cortex',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo_256.png',
         },
         items: [
           {
