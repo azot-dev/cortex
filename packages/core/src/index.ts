54cc593ad21243d94c93a2af604702e25bb0862e
Xavier Le Cunff <xavier@MacBook-Pro-de-Xavier.local>
2024-01-16 20:30:05 -0800

    fix: attempt to push new version

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 import { createCortexFactory } from "./coreFactory";
 import { BaseService } from "./base";
-import { Adapter, Gateway } from "./adapters";
+import { Adapter, Gateway } from "./adapters/adapters";
 
 export { createCortexFactory, BaseService, Adapter };
 export type { Gateway };
