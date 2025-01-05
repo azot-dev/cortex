7e1cb329f4fefe5967637eed7be8004331985918
Xavier Le Cunff <xavier.le-cunff@azot.dev>
2025-01-05 19:14:12 +0100

    feat: add valtio

--- packages/core/src/base-service.ts
+++ packages/core/src/base-service.ts
@@ -1,11 +1,12 @@
 import { Observable } from "@legendapp/state";
 import { ConstructedServiceTypes, GetStore, InferStoreType, ServiceConstructor } from "./types/service-constructor";
 import { ServiceRegistry } from "./service-registry";
+import { proxy } from "valtio";
 
 export abstract class BaseService<State, ServiceConstructorsType extends Record<string, ServiceConstructor<any, any, DependenciesType>>, DependenciesType> {
   constructor(
-    _store: Observable<GetStore<ServiceConstructorsType>>,
-    protected state: Observable<State>,
+    _store: GetStore<ServiceConstructorsType>,
+    protected state: State,
     protected dependencies: DependenciesType,
     private serviceRegistry: ServiceRegistry<ServiceConstructorsType, InferStoreType<ServiceConstructorsType>, DependenciesType>
   ) {
@@ -17,12 +18,17 @@ export abstract class BaseService<State, ServiceConstructorsType extends Record<
   init() {}
 
   public getState(): State {
-    return this.state.peek();
+    return this.state;
   }
 
   public setState(state: State | ((currentState: State) => State)) {
+    if (typeof state === "function") {
+      // @ts-ignore
+      this.state = proxy(state(this.state));
+      return;
+    }
     // @ts-ignore
-    this.state.set(state);
+    this.state = proxy(state);
   }
 
   protected getService<K extends keyof ServiceConstructorsType>(
