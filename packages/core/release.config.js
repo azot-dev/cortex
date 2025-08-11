37ec4f3ff49ed238708110951ac1b0887ce4155a
Xavier <xavier.lecunff@passculture.app>
2025-08-12 01:34:04 +0200

    next: use valtio

--- packages/core/release.config.js
+++ packages/core/release.config.js
@@ -19,7 +19,7 @@ module.exports = {
           { type: "refactor", release: "patch" },
           { type: "bump", release: "patch" },
           { type: "feat", release: "minor" },
-          { branch: "next", release: "major" },
+          { type: "next", release: "major" },
         ],
         preset: "angular",
       },
