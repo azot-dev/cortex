bf81ec6a1881f54bd7f3a1e8645cbc0901c0ea2c
Xavier <xavier.lecunff@passculture.app>
2025-08-12 01:07:39 +0200

    feat: new architecture
    BREAKING CHANGE: The API has been completely refactored.
    All methods have changed names and signatures.

--- packages/core/release.config.js
+++ packages/core/release.config.js
@@ -19,7 +19,6 @@ module.exports = {
           { type: "refactor", release: "patch" },
           { type: "bump", release: "patch" },
           { type: "feat", release: "minor" },
-          { branch: "next", release: "major" },
         ],
         preset: "angular",
       },
